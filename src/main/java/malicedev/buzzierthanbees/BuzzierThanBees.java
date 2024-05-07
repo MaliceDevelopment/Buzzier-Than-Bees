ed2d171af75e2f2c3a83c9513f9614628e913241
midford <Killer_Mid@outlook.com>
2024-05-07 23:06:15 +0300

    0.0.1-dev0001

---
+++ src/main/java/malicedev/buzzierthanbees/BuzzierThanBees.java
@@ -0,0 +1,46 @@
+package malicedev.buzzierthanbees;
+
+import net.fabricmc.api.ModInitializer;
+import net.minecraft.core.block.Block;
+import net.minecraft.core.block.BlockRotatableHorizontal;
+import net.minecraft.core.block.material.Material;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import turniplabs.halplibe.helper.BlockBuilder;
+import turniplabs.halplibe.util.GameStartEntrypoint;
+import turniplabs.halplibe.util.RecipeEntrypoint;
+
+
+public class BuzzierThanBees implements ModInitializer, GameStartEntrypoint, RecipeEntrypoint {
+    public static final String MOD_ID = "buzzierthanbees";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+	public static final Block hive = new BlockBuilder(MOD_ID)
+		.build(new BlockRotatableHorizontal("hive",2000, Material.wood){{}});
+    @Override
+    public void onInitialize() {
+        LOGGER.info("BuzzierthanBees initialized.");
+    }
+
+
+	@Override
+	public void beforeGameStart() {
+
+	}
+
+	@Override
+	public void afterGameStart() {
+
+	}
+
+	@Override
+	public void onRecipesReady() {
+
+	}
+
+	@Override
+	public void initNamespaces() {
+
+	}
+}
+
+
